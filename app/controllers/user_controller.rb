403e9c147e97ef7ddd76867a251457812662551b
Alabi12 <wolo22001@gmail.com>
2023-03-21 00:03:32 +0000

    Install devise and setup the user input

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+    def index
+      # code to display a list of users
+    end
+  
+    def show
+      # code to display a single user
+    end
+  
+    def create
+      # code to create a new user
+    end
+  
+    def update
+      # code to update an existing user
+    end
+  
+    def destroy
+      # code to delete a user
+    end
+  end
+  
